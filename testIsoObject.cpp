55707ee805d54b3042d0ac22b9f883e2f0082238
Oscar <oscar.johansson@astazero.com>
2021-05-10 08:27:58 +0200

    removed a unused line

--- testIsoObject.cpp
+++ testIsoObject.cpp
@@ -93,7 +93,6 @@ private:
 int main(int c, char** argv ) {
 
 	myObject obj;
-    obj.getTrajectory();
     while (true) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));  
     }
