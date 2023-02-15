2be1f6b90e4869d3fa87d705b4c0f597e719102e
Oscar Johansson <oscar.johansson@astazero.com>
2023-02-15 10:14:41 +0100

    Updated Python SWIG demo file. Updated ISO22133 ref.

--- swig/python_swig_testapp.py
+++ swig/python_swig_testapp.py
@@ -1,10 +1,12 @@
-from isoObject_wrap import TestObject, CartesianPosition, SpeedType
+from isoObject_wrap import TestObject, CartesianPosition, \
+    SpeedType, AccelerationType
 from time import sleep
 
 
 class myTestObject(TestObject):
 
-    # This is now overridden in the target language (Python)
+    # This is now overridden in the target language (Python).
+    # The "director" feature of SWIG is used to achieve this.
     def handleAbort(self):
         print("handleAbort() from python!")
 
@@ -12,6 +14,7 @@ class myTestObject(TestObject):
 if __name__ == "__main__":
 
     isoObject = myTestObject()
+
     pos = CartesianPosition()
     pos.xCoord_m = 0
     pos.yCoord_m = 0
@@ -19,14 +22,31 @@ if __name__ == "__main__":
     pos.heading_rad = 0
     pos.isHeadingValid = True
     pos.isPositionValid = True
-        
+    pos.isXcoordValid = True
+    pos.isYcoordValid = True
+    pos.isZcoordValid = True
+
     spd = SpeedType()
     spd.lateral_m_s = 0
     spd.longitudinal_m_s = 0
     spd.isLateralValid = True
     spd.isLongitudinalValid = True
+
+    acc = AccelerationType()
+    acc.isLateralValid = True
+    acc.isLongitudinalValid = True
+    acc.lateral_m_s2 = 0
+    acc.longitudinal_m_s2 = 0
+
     isoObject.setPosition(pos)
     isoObject.setSpeed(spd)
 
-    while(True):
-       sleep(1)
+    x = -1.0
+    y = 1.0
+    while (True):
+        sleep(1)
+        x -= 1.0
+        y += 1.0
+        pos.xCoord_m = x
+        pos.yCoord_m = y
+        isoObject.setPosition(pos)
